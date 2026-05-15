17d3fbf346b34585910a1f05a583fad688ee8a7d
TimothyBear11 <timothybear1183@pm.me>
2026-05-15 19:40:44 -0400

    simple update

--- terminal/fish.nix
+++ terminal/fish.nix
@@ -4,15 +4,43 @@
   # Make the fish shell binary available system-wide
   programs.fish.enable = true;
 
-  # Tell Hjem to take your existing file and live-symlink it
   hjem.users.tbear = {
-    # Fix: Stepping back out of the terminal folder to hit your central configs folder
-    files.".config/fish/config.fish".source = ../configs/fish/config.fish;
+    # Main shell file config
+    xdg.config.files."fish/config.fish" = {
+      source = ../configs/fish/config.fish;
+      clobber = true;
+    };
     
     # Keeps your plugin tracking profile active
-    files.".config/fish/fish_plugins".text = ''
-      jorgebucaran/fisher
-      IlanCosman/tide@v6
-    '';
+    xdg.config.files."fish/fish_plugins" = {
+      clobber = true;
+      text = ''
+        jorgebucaran/fisher
+        IlanCosman/tide@v6
+      '';
+    };
+
+    # Core frozen shell mapping bindings
+    xdg.config.files."fish/conf.d/fish_frozen_key_bindings.fish" = {
+      source = ../configs/fish/conf.d/fish_frozen_key_bindings.fish;
+      clobber = true;
+    };
+
+    # --- CUSTOM RUNTIME FUNCTIONS ---
+    xdg.config.files."fish/functions/fupdate-safe.fish".source    = ../configs/fish/functions/fupdate-safe.fish;
+    xdg.config.files."fish/functions/git-safe.fish".source        = ../configs/fish/functions/git-safe.fish;
+    xdg.config.files."fish/functions/goodnight.fish".source       = ../configs/fish/functions/goodnight.fish;
+    xdg.config.files."fish/functions/music-next.fish".source      = ../configs/fish/functions/music-next.fish;
+    xdg.config.files."fish/functions/music-pause.fish".source     = ../configs/fish/functions/music-pause.fish;
+    xdg.config.files."fish/functions/music-peek.fish".source      = ../configs/fish/functions/music-peek.fish;
+    xdg.config.files."fish/functions/music-play.fish".source      = ../configs/fish/functions/music-play.fish;
+    xdg.config.files."fish/functions/music-prev.fish".source      = ../configs/fish/functions/music-prev.fish;
+    xdg.config.files."fish/functions/music-stop.fish".source      = ../configs/fish/functions/music-stop.fish;
+    xdg.config.files."fish/functions/nix-clean.fish".source       = ../configs/fish/functions/nix-clean.fish;
+    xdg.config.files."fish/functions/nix-du.fish".source          = ../configs/fish/functions/nix-du.fish;
+    xdg.config.files."fish/functions/nrs.fish".source             = ../configs/fish/functions/nrs.fish;
+    xdg.config.files."fish/functions/play.fish".source            = ../configs/fish/functions/play.fish;
+    xdg.config.files."fish/functions/shell-switch.fish".source    = ../configs/fish/functions/shell-switch.fish;
+    xdg.config.files."fish/functions/gitnrs.fish".source          = ../configs/fish/functions/gitnrs.fish;
   };
 }
