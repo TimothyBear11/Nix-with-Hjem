742a57698c093e9ea766f7d66e218ce94d34a5ec
TimothyBear11 <timothybear1183@pm.me>
2026-05-15 19:34:40 -0400

    updated

--- emacs.nix
+++ emacs.nix
@@ -3,7 +3,7 @@
 {
   # Install Emacs and the companion search utilities Doom relies on
   environment.systemPackages = [
-    pkgs.emacs29-pgtk # Wayland-native pure GTK Emacs (perfect for Hyprland)
+    pkgs.emacs-pgtk # Wayland-native pure GTK Emacs (perfect for Hyprland)
     pkgs.ripgrep      # Fast text searching inside files (Doom project search)
     pkgs.fd           # Fast file finder
     pkgs.git          # Required for Doom to pull down its package modules
