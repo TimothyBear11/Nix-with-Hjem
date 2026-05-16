710716404589085f4974b9043b0e935602e49141
TimothyBear11 <timothybear1183@pm.me>
2026-05-15 20:20:57 -0400

    simple update

--- desktops/hyprland.nix
+++ desktops/hyprland.nix
@@ -1,4 +1,7 @@
-programs.hyprland = {
+{ config, pkgs, inputs, ... }:
+
+{
+  programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
