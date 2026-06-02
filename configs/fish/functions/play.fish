6ce4b0455ea1026f2f82ebf12e896128c4b5677b
TimothyBear11 <timothybear1183@pm.me>
2026-06-02 19:14:09 -0400

    simple update

--- configs/fish/functions/play.fish
+++ configs/fish/functions/play.fish
@@ -1,13 +1,6 @@
 function play
-    # Reference the file in your tracked nix directory
     set -l index_file ~/nix/configs/guardian/playlists.txt
-
-    if not test -f $index_file
-        echo "No playlists imported yet. Use 'music-import'."
-        return 1
-    end
-
-    set -l entry (grep "^$argv[1]|" $index_file)
+    set -l entry (grep "^$argv[1]|" $index_file | tail -n 1)
 
     if test -z "$entry"
         echo "Playlist '$argv[1]' not found."
@@ -19,6 +12,7 @@ function play
     tmux kill-session -t music 2>/dev/null
     pkill mpv 2>/dev/null
 
+    # Use a direct exec approach which is safer for URLs
     tmux new-session -d -s music "mpv --no-video --force-window=no --shuffle --input-ipc-server=/tmp/mpvsocket '$url'"
 
     echo "󰎈 Guardian Music Engine: Shuffling '$argv[1]'..."
