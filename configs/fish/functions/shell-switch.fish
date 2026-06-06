6c54f562bb388abe903685c3e950ae91ca4fe52b
TimothyBear11 <timothybear1183@pm.me>
2026-06-06 15:27:22 -0400

    simple update

--- configs/fish/functions/shell-switch.fish
+++ configs/fish/functions/shell-switch.fish
@@ -1,17 +1,24 @@
 #!/usr/bin/env fish
+# ~/.config/hypr/scripts/shell-switcher.fish
 
-set -l selection (printf "ambxst\ncaelestia\ndms\nnoctalia\nwayle" | fuzzel --dmenu -p "Select Shell: ")
+# 1. Force the selection into a variable, strip whitespace
+set -l selection (printf "ambxst\ncaelestia\ndms\nnoctalia\nnoctaliav5\nwayle" | fuzzel --dmenu -p "Select Shell: " | string trim)
+
+# If they hit ESC, just exit
 if test -z "$selection"
     exit 0
 end
 
-# 1. Kill the current shell explicitly BEFORE writing the file
-pkill -9 -x noctalia-shell
-pkill -9 -x wayle
-# ... (add other shell names here as needed)
+# 2. Kill the previous shell session variants
+# We use pkill -f to catch any backgrounded python/lua/qml wrappers
+pkill -9 -f noctalia
+pkill -9 -f wayle
+pkill -9 -f ambxst
+pkill -9 -f dms
+pkill -9 -f caelestia
 
-# 2. Update the state file
-echo "$selection" >~/.config/hypr/.active_shell
+# 3. Write the selection to the state file
+echo "$selection" >/home/tbear/.config/hypr/.active_shell
 
-# 3. Trigger reload
-hyprctl reload
+# 4. Trigger the reload (absolute path)
+/run/current-system/sw/bin/hyprctl reload
