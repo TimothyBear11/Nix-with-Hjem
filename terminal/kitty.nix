a59e46749961ed6f8a6f1391e49ea6467d97b10a
TimothyBear11 <timothybear1183@pm.me>
2026-09-24 02:19:33 -0400

    simple update

--- terminal/kitty.nix
+++ terminal/kitty.nix
@@ -6,7 +6,7 @@
 
   # 2. Let Hjem handle your configuration layout declaratively
   hjem.users.tbear = {
-    clobber = true;
+    clobberFiles = true;
     files.".config/kitty/kitty.conf".text = ''
       # ==========================================
       # Window Layout & Behavior
